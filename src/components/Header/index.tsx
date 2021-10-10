5176b19de1a64d5515537a71cb63b98510610806
Lucas Frantz <maxkronus@gmail.com>
2021-10-10 14:31:41 -0300

    config

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss'
-export function Header(){
-    return(
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news" />
-                <nav>
-                    <a className={styles.active} href="">Home</a>
-                    <a href="">Posts</a>
-                </nav>
-                <SignInButton/>
-            </div>
-        </header>
-    );
+import styles from './styles.module.scss';
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a className={styles.active} href="">
+            Home
+          </a>
+          <a href="">Posts</a>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
 }
---
+++ src/components/test.ts
@@ -0,0 +1,2 @@
+export const APP_VERSION = "v1.0.0"
+
