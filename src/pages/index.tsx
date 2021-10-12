13f5a74218eee56b4f4b9097f4223c0c392094f6
Lucas Frantz <maxkronus@gmail.com>
2021-10-11 21:44:55 -0300

    listar posts do prismic cms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,21 @@
-import {GetStaticProps} from 'next'
-import Head from 'next/head'
-import { SubscribeButton } from '../components/SubscribeButton'
-import { stripe } from '../services/stripe'
-import styles from './home.module.scss'
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+import styles from './home.module.scss';
 
 //Client-side
 //Server-side
 //Static Site Generation
 
-interface HomeProps{
+interface HomeProps {
   product: {
     priceId: string;
     amount: number;
-  }
+  };
 }
 
-
-export default function Home({product} : HomeProps) {
-  
+export default function Home({ product }: HomeProps): JSX.Element {
   return (
     <>
       <Head>
@@ -26,38 +24,36 @@ export default function Home({product} : HomeProps) {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
-          <h1>News about the <span>React</span> world.</h1>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
           <p>
-            Get access to all the publications <br/>
+            Get access to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId}/>
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
-  )
+  );
 }
 
-
-export const getStaticProps : GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve('price_1JipeBFICFmY72ggG8eIXSsg')
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JipeBFICFmY72ggG8eIXSsg');
 
   const product = {
     priceId: price.id,
-    amount: new Intl.NumberFormat(
-      'en-US',
-      {
-        style: 'currency',
-        currency: 'USD'
-      }
-    ).format(price.unit_amount / 100)
-  }
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
 
   return {
     props: {
-      product
+      product,
     },
-    revalidate: 60*60*24, // 24 hours
-  }
-}
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
